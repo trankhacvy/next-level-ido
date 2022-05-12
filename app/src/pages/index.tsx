a1df0d34102b48952f2aef7e87a8cfae87bdaf8e
Khac Vy <khacvy93@gmail.com>
2022-05-12 20:12:54 +0700

    chore: try 4

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,16 @@
+import { InferGetStaticPropsType } from "next";
 import Head from "next/head";
 import Hero from "views/home/Hero";
+import Projects from "components/Projects";
 import Statistic from "views/home/Statistic";
 import StartNow from "views/home/StartNow";
 import TiersSection from "views/home/TiersSection";
+import { Project } from "types/common";
+import { ProjectsRepositoty } from "libs/supabase";
 
 const Index = ({
-}) => {
+  projects,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <main>
       <Head>
@@ -17,32 +22,32 @@ const Index = ({
       </Head>
       <Hero />
       <Statistic />
-      {/* <Projects projects={projects} /> */}
+      <Projects projects={projects} />
       <TiersSection />
       <StartNow />
     </main>
   );
 };
 
-// export const getStaticProps = async () => {
-//   try {
-//     const repo = new ProjectsRepositoty();
-//     const projects = await repo.findByStatus("live", 6);
-//     console.log('[index] getStaticProps ', projects.length)
-//     return {
-//       props: {
-//         projects: projects as Project[],
-//       },
-//       revalidate: 60,
-//     };
-//   } catch (error) {
-//     return {
-//       props: {
-//         projects: [] as Project[],
-//       },
-//       revalidate: 60,
-//     };
-//   }
-// };
+export const getStaticProps = async () => {
+  try {
+    const repo = new ProjectsRepositoty();
+    const projects = await repo.findByStatus("live", 6);
+    console.log('[index] getStaticProps ', projects.length)
+    return {
+      props: {
+        projects: projects as Project[],
+      },
+      revalidate: 60,
+    };
+  } catch (error) {
+    return {
+      props: {
+        projects: [] as Project[],
+      },
+      revalidate: 60,
+    };
+  }
+};
 
 export default Index;
